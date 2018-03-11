b2d4397e081420581d00fa695925dc1842a5d49d
parkerhams <parker.hamilton@loop.colum.edu>
2018-03-11 18:36:29 -0500

    Movement using Character Controller, no turning; imported more assets, test shoot laser

---
+++ Assets/Scripts/NarwhalMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NarwhalMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 10f;
+    [SerializeField]
+    float jumpForce = 15f;
+    [SerializeField]
+    float gravity = 30f;
+    [SerializeField]
+    Vector3 moveDirection = Vector3.zero;
+
+    private Rigidbody narwhalRigidbody;
+    private Text playerNumberText;
+    private CharacterController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        narwhalRigidbody = GetComponent<Rigidbody>();
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveAndJump();
+    }
+
+    private void PlayerMoveAndJump()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            moveDirection *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
+
+
