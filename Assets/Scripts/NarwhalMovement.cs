f11a537dd2fe1885a087cd67305a96523e8cc189
parkerhams <parker.hamilton@loop.colum.edu>
2018-03-11 21:22:41 -0500

    Evironment adjustments, camera rig

--- Assets/Scripts/NarwhalMovement.cs
+++ Assets/Scripts/NarwhalMovement.cs
@@ -8,6 +8,8 @@ public class NarwhalMovement : MonoBehaviour
 
     [SerializeField]
     float speed = 10f;
+    //[SerializeField]
+    //float rotateSpeed = 5f;
     [SerializeField]
     float jumpForce = 15f;
     [SerializeField]
@@ -16,19 +18,33 @@ public class NarwhalMovement : MonoBehaviour
     Vector3 moveDirection = Vector3.zero;
 
     private Rigidbody narwhalRigidbody;
+    int playerNumber_UseProperty;
     private Text playerNumberText;
     private CharacterController controller;
+    //float horizontalInput;
+    //float verticalInput;
+    //string horizontalAxis;
+    //string verticalAxis;
+
+    //public int PlayerNumber
+    //{
+    //    get { return playerNumber_UseProperty; }
+    //    set { playerNumber_UseProperty = value; }
+    //}
 
     // Use this for initialization
     void Start()
     {
         narwhalRigidbody = GetComponent<Rigidbody>();
         controller = gameObject.GetComponent<CharacterController>();
+        //horizontalAxis = "P" + PlayerNumber + "-Horizontal";
+        //verticalAxis = "P" + PlayerNumber + "-Vertical";
     }
 
     // Update is called once per frame
     void Update()
     {
+        //HandleInput();
         PlayerMoveAndJump();
     }
 
@@ -51,6 +67,21 @@ public class NarwhalMovement : MonoBehaviour
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
     }
+
+    //void Rotate()
+    //{
+    //    float turn = horizontalInput * rotateSpeed * Time.fixedDeltaTime;
+    //    Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+    //    narwhalRigidbody.angularVelocity = Vector3.zero;
+    //    narwhalRigidbody.MoveRotation(narwhalRigidbody.rotation * turnRotation);
+    //}
+
+    //void HandleInput()
+    //{
+    //    horizontalInput = Input.GetAxis("Horizontal");
+    //    verticalInput = Input.GetAxis("Vertical");
+    //    //shouldShoot = Input.GetButtonDown(fireButton);
+    //}
 }
 
 
