3e51390b8c7d34cd82f925fa64070843a8630f46
parkerhams <parker.hamilton@loop.colum.edu>
2018-03-25 21:59:30 -0500

    CreatedDifferentSceneForTitleMenu, tried Worldspace UI but too much too learn - stretch goal for portfolio. Visualizer Orbit too

--- Assets/Scripts/DynamicBackgrounds.cs
+++ Assets/Scripts/DynamicBackgrounds.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallaxing : MonoBehaviour {
+public class DynamicBackgrounds : MonoBehaviour {
 
     [SerializeField]
     Transform cameraPos;
